c51ac71c3b73cce9ec1757d1e559465c033cdca9
Admin <admin@admins-macbook-pro.local>
2016-04-06 21:57:44 -0400

    Initial Commit

---
+++ SocialTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialTracker
+//
+//  Created by Admin on 4/6/16.
+//  Copyright © 2016 Flash Software Solution Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
